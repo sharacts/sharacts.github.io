6b04221f72fd7dd5fa05c87875fb46b7348ca0c4
Leigh Silverstein <l2silver@ryerson.ca>
2020-09-30 19:18:54 -0400

    nextCommit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85e56cabbf6eae2c3424b45753afc2c7.js"
+  "/precache-manifest.899ab2ba19fe0a0377450996a9e551a4.js"
 );
 
 self.addEventListener('message', (event) => {
